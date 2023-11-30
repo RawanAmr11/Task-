74d8c16a67797b49d99207ebd4d2b4811c868b65
RawanAmr11 <rawan.amr304@gmail.com>
2023-11-30 09:49:39 +0200

    First Commit

---
+++ FacultyWPF/FacultyWPF/ApplicationPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FacultyWPF
+{
+    /// <summary>
+    /// Interaction logic for ApplicationPage.xaml
+    /// </summary>
+    public partial class ApplicationPage : Page
+    {
+        public ApplicationPage()
+        {
+            InitializeComponent();
+        }
+        
+
+        private void OpenData_Click(object sender, RoutedEventArgs e)
+        {
+            DataListPage dataListPage = new DataListPage();
+            this.NavigationService.Navigate(dataListPage);
+        }
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            FacultyEntities1 db = new FacultyEntities1();
+            Student student = new Student();
+            student.Name = nameTxt.Text;
+            student.Address = addTxt.Text;
+            student.Age = int.Parse(ageTxt.Text);
+            db.Students.Add(student);
+            db.SaveChanges();
+            MessageBox.Show("Done");
+            nameTxt.Text = "";
+            addTxt.Text = "";
+            ageTxt.Text = "";
+        }
+
+        private void ageTxt_Copy_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ FacultyWPF/FacultyWPF/DataListPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FacultyWPF
+{
+    /// <summary>
+    /// Interaction logic for DataListPage.xaml
+    /// </summary>
+    public partial class DataListPage : Page
+    {
+        FacultyEntities1 db = new FacultyEntities1();
+        public DataListPage()
+        {
+            InitializeComponent();
+            dg.ItemsSource = db.Students.ToList();  
+        }
+
+        private void RefreshBtn(object sender, RoutedEventArgs e)
+        {
+            dg.ItemsSource = db.Students.ToList();
+        }
+
+        private void SearchBtn(object sender, RoutedEventArgs e)
+        {
+            var subArray = Combo.SelectedItem.ToString().Split(' ');
+            string dep = subArray.Last();
+            dg.ItemsSource = db.Students.Where(x => x.Name.Contains(searchByName.Text) && x.DepName == dep).ToList();
+            
+        }
+
+    }
+}
